5a1415118ec7ecb90d48ca68ea80dc7162b1e503
semiste <semistro@hotmail.nl>
2024-08-05 14:42:45 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', function () {
     const questionForm = document.getElementById('question-form');
     const timestampElement = document.getElementById('timestamp');
     const delayBeforeNextQuestion = 1000; // Adjust delay to match the GIF animation time
-    const googleWebAppURL = 'https://script.google.com/macros/s/AKfycbxh65DBy8ZuLr27bwjkJCWLkUt7KdZ_m7YTYBLE8LM/dev'; // Replace with your Google Apps Script Web App URL
+    const googleWebAppURL = 'https://script.google.com/macros/s/AKfycbwYALiVz5lqQWp-j-MKaWmtnliyZIPt2q8E84jYP5t8Nu9j2oj8Bt4WOV0ntjKOQeYN/exec'; // Replace with your Google Apps Script Web App URL
 
     let startTime;
 
@@ -19,31 +19,23 @@ document.addEventListener('DOMContentLoaded', function () {
     }
 
     function startBackgroundMusic() {
-        if (backgroundMusic) {
-            backgroundMusic.play().catch(error => {
-                console.error('Error playing background music:', error);
-            });
-        } else {
-            console.error('Background music element not found.');
-        }
+        backgroundMusic.play().catch(error => {
+            console.error('Error playing background music:', error);
+        });
     }
 
     function updateTimestamp() {
-        if (timestampElement) {
-            fetch('https://api.github.com/repos/semiste/widm/commits?per_page=1')
-                .then(response => response.json())
-                .then(data => {
-                    const lastCommitDate = new Date(data[0].commit.committer.date);
-                    const options = { year: 'numeric', month: 'long', day: 'numeric', hour: '2-digit', minute: '2-digit', second: '2-digit' };
-                    timestampElement.textContent = `Last build time: ${lastCommitDate.toLocaleDateString('en-US', options)}`;
-                })
-                .catch(error => {
-                    console.error('Error fetching last commit time:', error);
-                    timestampElement.textContent = 'Last build time: Unable to fetch';
-                });
-        } else {
-            console.error('Timestamp element not found.');
-        }
+        fetch('https://api.github.com/repos/semiste/widm/commits?per_page=1')
+            .then(response => response.json())
+            .then(data => {
+                const lastCommitDate = new Date(data[0].commit.committer.date);
+                const options = { year: 'numeric', month: 'long', day: 'numeric', hour: '2-digit', minute: '2-digit', second: '2-digit' };
+                timestampElement.textContent = Last build time: ${lastCommitDate.toLocaleDateString('en-US', options)};
+            })
+            .catch(error => {
+                console.error('Error fetching last commit time:', error);
+                timestampElement.textContent = 'Last build time: Unable to fetch';
+            });
     }
 
     function showQuestion(index) {
@@ -55,45 +47,41 @@ document.addEventListener('DOMContentLoaded', function () {
 
     function displayQuestions(questions) {
         const container = document.getElementById('question-form');
-        if (container) {
-            container.innerHTML = '';
+        container.innerHTML = '';
 
-            questions.forEach((question, index) => {
-                const questionDiv = document.createElement('div');
-                questionDiv.className = 'question';
-                questionDiv.id = `question-${index}`;
-                questionDiv.style.display = index === 0 ? 'block' : 'none';
+        questions.forEach((question, index) => {
+            const questionDiv = document.createElement('div');
+            questionDiv.className = 'question';
+            questionDiv.id = question-${index};
+            questionDiv.style.display = index === 0 ? 'block' : 'none';
 
-                const questionHTML = `
-                    <img src="resources/background_exam.png" alt="Background Exam" class="background-image">
-                    <div class="question-container">
-                        <h2 style="color: white;">${question.text}</h2>
-                        ${question.options.map(option => `
-                            <button class="choice-button" data-answer="${option}">
-                                <img src="resources/Button.png" class="button-img" data-state="default">
-                                <span>${option}</span>
-                            </button>
-                        `).join('')}
-                    </div>
-                `;
+            const questionHTML = 
+                <img src="resources/background_exam.png" alt="Background Exam" class="background-image">
+                <div class="question-container">
+                    <h2 style="color: white;">${question.text}</h2>
+                    ${question.options.map(option => 
+                        <button class="choice-button" data-answer="${option}">
+                            <img src="resources/Button.png" class="button-img" data-state="default">
+                            <span>${option}</span>
+                        </button>
+                    ).join('')}
+                </div>
+            ;
 
-                questionDiv.innerHTML = questionHTML;
-                container.appendChild(questionDiv);
-            });
+            questionDiv.innerHTML = questionHTML;
+            container.appendChild(questionDiv);
+        });
 
-            document.querySelectorAll('.choice-button').forEach(button => {
-                button.addEventListener('click', handleChoiceClick);
-            });
-        } else {
-            console.error('Question form container not found.');
-        }
+        document.querySelectorAll('.choice-button').forEach(button => {
+            button.addEventListener('click', handleChoiceClick);
+        });
     }
 
     function handleChoiceClick(event) {
         const button = event.currentTarget;
         const img = button.querySelector('.button-img');
         img.src = 'resources/aButton.gif'; // Change to gif image
-
+        
         // Play sound effect
         const clickSound = new Audio('resources/klik.wav');
         clickSound.play();
@@ -111,35 +99,32 @@ document.addEventListener('DOMContentLoaded', function () {
         }, delayBeforeNextQuestion);
     }
 
-function submitFormData() {
-    const formData = {
-        name: document.getElementById('name').value,
-        answers: [],
-        timeTaken: getTimeTaken() // Get the time taken to complete the test
-    };
+    function submitFormData() {
+        const formData = {
+            name: document.getElementById('name').value,
+            answers: [],
+            timeTaken: getTimeTaken() // Get the time taken to complete the test
+        };
 
-    document.querySelectorAll('.choice-button.selected').forEach(button => {
-        formData.answers.push(button.dataset.answer);
-    });
+        document.querySelectorAll('.choice-button.selected').forEach(button => {
+            formData.answers.push(button.dataset.answer);
+        });
 
-    fetch(googleWebAppURL, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(formData)
-    })
-    .then(response => response.text())
-    .then(text => {
-        alert('Test submitted successfully!');
-        console.log(text);
-    })
-    .catch(error => {
-        console.error('Error:', error);
-        alert('There was an error submitting your test.');
-    });
-}
-
+        fetch(googleWebAppURL, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(formData)
+        }).then(response => response.text())
+        .then(text => {
+            alert('Test submitted successfully!');
+            console.log(text);
+        }).catch(error => {
+            console.error('Error:', error);
+            alert('There was an error submitting your test.');
+        });
+    }
 
     startButton.addEventListener('click', function () {
         const name = document.getElementById('name').value;
